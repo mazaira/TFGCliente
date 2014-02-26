8247eeddd7d6991c0ed44960f23f897f1f50d667
mazaira <a.mmazaira@gmail.com>
2014-02-26 14:02:06 +0000

    Create Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TFG::Application.config.secret_key_base = '7eb6373075b08396dc45410472c5b2487af3ae1c0c2845644320ef9b4817e1a656f69186438af467e0f0a977631bc1bc6791df7748c8b6025175f2318141d359'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TFG::Application.config.session_store :cookie_store, key: '_TFG_session'
