5b129e0b76ce840657cf621b1b4aaa05f43da5c1
mazaira <a.mmazaira@gmail.com>
2014-05-24 15:19:20 +0200

    actualizar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TFG::Application.routes.draw do
+  resources :products
+
   get "main/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
